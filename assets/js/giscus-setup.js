f324170754d570a327aa2076ac80fcca03d92999
SaiRakeshPeteru <n200764@rguktn.ac.in>
2025-09-16 05:50:09 +0000

    Deploying to gh-pages from @ SaiRakeshPeteru/RakeshPeteru.github.io@3fa513b031476f42d855436d90b0addfcded2e6a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SaiRakeshPeteru/Rakesh_Peteru.github.io",
+    "data-repo": "SaiRakeshPeteru/RakeshPeteru.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
